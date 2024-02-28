39ebab40dfa248383475cf0c7c8a989b0b893175
Flibouche <kevin.pfiffer2@gmail.com>
2024-02-28 16:52:00 +0100

    fin exercice 7

--- exercice7.php
+++ exercice7.php
@@ -2,4 +2,24 @@
 
 echo "<h1>Exercice n°7</h1>";
 
-$age = 10;
+$age = 7;
+
+if($age == 6 or $age == 7) {
+    $result = "Poussin";
+    echo "L'enfant qui a $age ans appartient à la catégorie $result.<br>";
+}
+elseif($age == 8 or $age == 9) {
+    $result = "Pupille";
+    echo "L'enfant qui a $age ans appartient à la catégorie $result.<br>";
+}
+elseif($age == 10 || $age == 11) {
+    $result = "Minime";
+    echo "L'enfant qui a $age ans appartient à la catégorie $result.<br>";
+}
+elseif($age == 12) {
+    $result = "Cadet";
+    echo "L'enfant qui a $age ans appartient à la catégorie $result.<br>";
+}
+else {
+    echo "La catégorie n'est pas gérée.<br>";
+}
